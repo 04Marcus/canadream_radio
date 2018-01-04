55f1eb4817add27a46f4bfc09f9e7dd1705abec8
04Marcus <35038131+04Marcus@users.noreply.github.com>
2018-01-04 23:56:52 +0100

    optical edit

--- stations/stations-canadream.js
+++ stations/stations-canadream.js
@@ -1,17 +1,17 @@
 var stations = {
 "baiecomeau": [
         {
-			name: "CBC Radio One 93.7 FM",
+	    name: "CBC Radio One 93.7 FM",
             logo: "stations/images-america/canadream/CBC Radio One Quebec CBVE.png",
             url: "http://cbc_r1_qqu.akacast.akamaistream.net/7/29/451661/v1/rc.akacast.akamaistream.net/cbc_r1_qqu"
 	    },
-		{
-			name: "fm 97.1",
+	    {
+	    name: "fm 97.1",
             logo: "stations/images-america/canadream/baiecomeau/fm 97.1.jpg",
             url: "http://podcast.servlinks.com:8416/;?type=http&nocache=142"
 	    },
-		{
-			name: "Premiere Chaine 106.1",
+	    {
+	    name: "Premiere Chaine 106.1",
             logo: "stations/images-america/canadream/Radio Premiere Chaine CBSI.png",
             url: "http://2qque0.akacast.akamaistream.net/7/896/177395/v1/rc.akacast.akamaistream.net/2QQUE0"
 	    },
