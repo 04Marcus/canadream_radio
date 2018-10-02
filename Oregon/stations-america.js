34d182f85b62501aebf91bc084ced88ab1e379bc
04Marcus <35038131+04Marcus@users.noreply.github.com>
2018-10-02 17:31:37 +0200

    see readme

--- Oregon/stations-america.js
+++ Oregon/stations-america.js
@@ -1,5 +1,20 @@
 "portland": [
         {
+            name: "Sports Radio 620 AM",
+            logo: "stations/images-america/portland/Sports Radio 620 AM.png",
+            url: "https://c3icy.prod.playlists.ihrhls.com/1965_icy"
+        },
+        {
+            name: "1190 KEX News Radio",
+            logo: "stations/images-america/portland/1190 KEX News Radio.png",
+            url: "https://c13icy.prod.playlists.ihrhls.com/1949_icy"
+        },
+        {
+            name: "Oregon Public Broadcasting 91.5 FM",
+            logo: "stations/images-america/portland/OPB.png",
+            url: "http://stream3.opb.org/radio.mp3"
+        },
+        {
             name: "Air1",
             logo: "stations/images-america/portland/Air1.png",
             url: "http://emf.streamguys1.com/sa001_mp3_high_web"
@@ -23,6 +38,21 @@
             name: "Z100 Portland",
             logo: "stations/images-america/portland/Z100.png",
             url: "http://c13icyelb.prod.playlists.ihrhls.com/1961_icy"
+        },
+        {
+            name: "105.9 The Brew",
+            logo: "stations/images-america/portland/105.9 The Brew.png",
+            url: "https://c13icy.prod.playlists.ihrhls.com/3540_icy"
+        },
+        {
+            name: "106.7 The Eagle",
+            logo: "stations/images-america/portland/106.7 The Eagle.png",
+            url: "https://c13icy.prod.playlists.ihrhls.com/4315_icy"
+        },
+        {
+            name: "Jam'n 107.5",
+            logo: "stations/images-america/portland/jamn1075.png",
+            url: "https://c13icy.prod.playlists.ihrhls.com/4319_icy"
         },
     ],
 		
