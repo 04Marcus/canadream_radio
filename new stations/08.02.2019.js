2220c8c25e774eab6fe483f8e0998cead3828d63
04Marcus <35038131+04Marcus@users.noreply.github.com>
2019-02-08 21:32:08 +0100

    see readme
    Ukiah rework

---
+++ new stations/08.02.2019.js
@@ -0,0 +1,37 @@
+    "ukiah": [
+        {
+            name: "Broken FM 88.7",
+            logo: "stations/images-america/ukiah/Broken FM.png",
+            url: "http://brokenfm.primcast.com:6834/;stream/1"
+        },
+        {
+            name: "Family Radio West Coast",
+            logo: "stations/images-america/ukiah/Family Radio West Coast.svg",
+            url: "http://playerservices.streamtheworld.com/api/livestream-redirect/FAMILYRADIO_WEST.mp3"
+        },
+        {
+            name: "KZYX 91.5",
+            logo: "stations/images-america/ukiah/KZYZ 91.5.png",
+            url: "http://kzyx.mcn.org:4000/kzyxlive"
+        },
+        {
+            name: "Max 93.5 FM",
+            logo: "stations/images-america/ukiah/Max 93.5 FM.jpg",
+            url: "http://crystalout.surfernetwork.com:8001/KMKX-FM_MP3"
+        },
+        {
+            name: "94.5 K-Wine",
+            logo: "stations/images-america/ukiah/94.5 K-Wine.jpg",
+            url: "http://crystalout.surfernetwork.com:8001/KWNE-FM_MP3"
+        },
+        {
+            name: "The Coast 95.3",
+            logo: "stations/images-america/ukiah/The Coast.png",
+            url: "http://54.82.44.170/caradio-koztfmmp3-ibc3"
+        }, 
+        {
+            name: "The Skunk FM 96.7",
+            logo: "stations/images-america/ukiah/The Skunk FM 96.7.png",
+            url: "http://streaming.rubinbroadcasting.com/kunk"
+        },
+    ],
