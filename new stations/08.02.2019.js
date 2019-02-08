549a14515187ffa707ae66830a250d57c940f177
04Marcus <35038131+04Marcus@users.noreply.github.com>
2019-02-08 22:00:33 +0100

    see readme

--- new stations/08.02.2019.js
+++ new stations/08.02.2019.js
@@ -1,4 +1,56 @@
-    "ukiah": [
+   
+
+    "eureka": [
+        {
+            name: "KMUD 88.1",
+            logo: "stations/images-america/eureka/KMUD.png",
+            url: "http://live.str3am.com:2470/live"
+        },
+        {
+            name: "KVIP Radio 89.3 FM",
+            logo: "stations/images-america/burns/KVIP Radio 89.5 FM.png",
+            url: "http://sc1.christiannetcast.com:9112/;"
+        },
+        {
+            name: "KHSU 90.5",
+            logo: "stations/images-america/eureka/KHSU 90.5.png",
+            url: "http://peace.str3am.com:6650/"
+        },
+        {
+            name: "JPR News",
+            logo: "stations/images-america/c2c/JPR Rhythm & News.png",
+            url: "http://jpr.streamguys.org/jpr-news"
+        },
+        {
+            name: "KSLG 93.1",
+            logo: "stations/images-america/eureka/KSLG.png",
+            url: "http://lostcoast.streamguys.us/kslg-hi"
+        },
+        {
+            name: "94.1 The Lounge",
+            logo: "stations/images-america/eureka/94.1 The Lounge.png",
+            url: "http://lostcoast.streamguys.us/the_lounge"
+        },
+        {
+            name: "K-LOVE",
+            logo: "stations/images-america/huron/K-LOVE.png",
+            url: "http://emf.streamguys1.com/sk001_mp3_high_web?tsid=1516209902878"
+        },
+        {
+            name: "KHUM 104.7",
+            logo: "stations/images-america/eureka/KHUM 104.7.svg",
+            url: "http://lostcoast.streamguys.us/khum-hi"
+        },
+        {
+            name: "KINS 106.3",
+            logo: "stations/images-america/eureka/KINS 106.3.png",
+            url: "http://eurekaradio.streamguys1.com/live"
+        },
+    ],
+
+
+
+   "ukiah": [
         {
             name: "Broken FM 88.7",
             logo: "stations/images-america/ukiah/Broken FM.png",
